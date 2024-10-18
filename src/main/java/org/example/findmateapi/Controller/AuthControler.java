feccccdc26359b3817c34d6414ff8d84ccffb125
wiktor <prokopiszynwiktor@gmail.com>
2024-10-18 14:56:26 +0200

    Fixes for web requests

--- src/main/java/org/example/findmateapi/Controller/AuthControler.java
+++ src/main/java/org/example/findmateapi/Controller/AuthControler.java
@@ -4,13 +4,11 @@ import org.example.findmateapi.Request.RegisterRequest;
 import org.example.findmateapi.Service.AuthUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:63342")
 public class AuthControler {
     @Autowired
     private AuthUserService authUserService;
