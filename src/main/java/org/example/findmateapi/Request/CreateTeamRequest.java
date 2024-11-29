2a86fe05844f32010dc6adb71e1468938287ca36
Wiktor <prokopiszynwiktor@gmail.com>
2024-11-29 01:42:51 +0100

    Team creation added

---
+++ src/main/java/org/example/findmateapi/Request/CreateTeamRequest.java
@@ -0,0 +1,16 @@
+package org.example.findmateapi.Request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateTeamRequest {
+    private String teamName;
+
+}
--- src/main/java/org/example/findmateapi/Service/AuthUserService.java
+++ src/main/java/org/example/findmateapi/Service/AuthUserService.java
@@ -23,7 +23,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -177,8 +176,10 @@ public class AuthUserService {
 
     private User getUserFromToken(String token){
         String username = jwtUtils.extractUsername(token);
-        return userRepository.findByUsername(username)
-                .orElse(null);
+        if (username == null || username.isEmpty()) {
+            return null;
+        }
+        return userRepository.findByUsername(username).orElse(null);
     }
 
     /**
