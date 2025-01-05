d488112a345e75914bef742b9ec21505e9fecedb
Hubert <hubert.muszalski@gmail.com>
2025-01-05 20:57:06 +0100

    Updating and refreshing League of Legends Profiles.

---
+++ src/main/java/org/example/findmateapi/Request/UpdateLolProfileRequest.java
@@ -0,0 +1,20 @@
+package org.example.findmateapi.Request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateLolProfileRequest {
+    private Integer rank;
+    private String summonerName;
+    private Boolean roleTop;
+    private Boolean roleJungle;
+    private Boolean roleMid;
+    private Boolean roleAdc;
+    private Boolean roleSupport;
+}
