5de2b506651630c6c10f7e8b72f0b5ac8024ee18
Wiktor <prokopiszynwiktor@gmail.com>
2025-01-14 19:56:18 +0100

    FIXED added sport prifles

--- src/main/java/org/example/findmateapi/Controller/SportProfileController.java
+++ src/main/java/org/example/findmateapi/Controller/SportProfileController.java
@@ -1,5 +1,7 @@
 package org.example.findmateapi.Controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import org.example.findmateapi.Entity.SportProfile;
 import org.example.findmateapi.Repository.SportProfileRepository;
@@ -15,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/sportprofile")
 @CrossOrigin(origins = "http://localhost:63342")
+@Tag(name = "SportProfileController", description = "Endpoints for managing sport profiles")
 public class SportProfileController {
 
     @Autowired
@@ -23,26 +26,27 @@ public class SportProfileController {
     private SportProfileRepository sportProfileRepository;
 
     @PostMapping("/create")
+    @Operation(summary = "Create a Sport Profile", description = "Creates a new sport profile for a user")
     public ResponseEntity<?> createSportProfile(@RequestBody CreateSportProfileRequest createSportProfileRequest, HttpServletRequest request) {
         return sportProfileService.createSportProfile(createSportProfileRequest, request);
     }
 
     @PostMapping("/refresh")
+    @Operation(summary = "Refresh a Sport Profile", description = "Refreshes the last refresh time of a user's sport profile")
     public ResponseEntity<?> refreshSportProfile(HttpServletRequest request) {
         return sportProfileService.refreshSportProfile(request);
     }
 
-
     @PostMapping("/search")
+    @Operation(summary = "Search Sport Profiles", description = "Searches for sport profiles based on filters")
     public ResponseEntity<?> searchSportProfiles(@RequestBody FilterSportProfilesRequest filterSportProfilesRequest, HttpServletRequest request) {
         return sportProfileService.searchSportProfiles(filterSportProfilesRequest, request);
     }
-    
+
     @GetMapping("/all")
+    @Operation(summary = "Get all Sport Profiles", description = "Retrieves all sport profiles")
     public ResponseEntity<?> getAllProfiles() {
         List<SportProfile> profiles = sportProfileRepository.findAll();
-
-        // Wrap in ResponseEntity with 200 OK status
         return ResponseEntity.ok(profiles);
     }
 }
